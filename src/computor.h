9613113c8c832a07247a00b1c28f58e6edd94392
Remy DESTREBECQ <rdestreb@e3r9p18.42.fr>
2015-03-31 16:17:49 +0200

    otter file

---
+++ src/computor.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   computor.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdestreb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/03/31 14:42:06 by rdestreb          #+#    #+#             */
+/*   Updated: 2015/03/31 16:14:20 by rdestreb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMPUTOR_H
+# define COMPUTOR_H
+
+# include "../libft/libft.h"
+# include <math.h>
+
+typedef struct s_coeff	t_coeff;
+
+struct	s_coeff
+{
+	int	z;
+	int	u;
+	int	d;
+	int	a;
+}
+
+#endif
